2d0b16517fae66bce6331730ee018368a35d0954
Anastasia951 <n.ivanova504@gmail.com>
2023-02-28 00:19:58 +0300

    add timer

--- src/components/Header/components/Timer/Timer.jsx
+++ src/components/Header/components/Timer/Timer.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { completeTo3Digits } from '../../../../helpers/completeTo3Digits'
 import { Digit } from '../../../Digit/Digit'
 import styles from './Timer.module.scss'
 export const Timer = () => {
+  const [seconds, setSeconds] = useState('000')
+  const timerRef = useRef()
+  function clear() {
+    clearInterval(timerRef.current)
+  }
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setSeconds(sec => {
+        let newValue = (Number(sec) + 1).toString()
+        if (newValue.length > 3) clear()
+        return completeTo3Digits(newValue)
+      })
+    }, 1000)
+
+    return clear
+  }, [])
+
   return (
     <div className={styles.timer}>
-      <Digit number={3} />
-      <Digit />
-      <Digit />
+      {seconds.split('').map((el, index) => (
+        <Digit key={index} number={el} />
+      ))}
     </div>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import { create } from "zustand";
 
-
 const FIELD_WIDTH = 16
 export const useStore = create((set) => ({
   field: new Array(FIELD_WIDTH).fill('').map(() => {
