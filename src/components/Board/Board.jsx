5724c5ed8faafd858a045cc313bee9734a4ff72b
Anastasia951 <n.ivanova504@gmail.com>
2023-02-27 23:22:11 +0300

    start ui

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { useStore } from '../../store/store'
 import { Cell } from '../Cell/Cell'
+import { Header } from '../Header/Header'
 import styles from './Board.module.scss'
 
 export const Board = () => {
   const board = useStore(state => state.field)
   console.log(board)
   return (
-    <div className={styles.grid}>
-      {board.map((row, _) => {
-        return row.map((__, index) => {
-          return <Cell key={index} />
-        })
-      })}
+    <div className={`${styles.game} bordered-revert`}>
+      <Header />
+      <div className={`${styles.grid} bordered`}>
+        {board.map((row, _) => {
+          return row.map((__, index) => {
+            return <Cell key={index} />
+          })
+        })}
+      </div>
     </div>
   )
 }
