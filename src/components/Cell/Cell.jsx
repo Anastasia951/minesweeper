1dd13636e8a5460c939bd411f2a19ef31c00138d
Anastasia951 <n.ivanova504@gmail.com>
2023-02-27 21:40:32 +0300

    add Cell component

---
+++ src/components/Cell/Cell.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './Cell.module.scss'
+export const Cell = ({ isOpened = false, value }) => {
+  let classname = ''
+  if (isOpened) {
+    switch (value) {
+      case -1: {
+        classname = 'bomb-opened'
+        break
+      }
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8: {
+        classname = `value-${value}`
+        break
+      }
+      default: {
+        classname = 'empty'
+      }
+    }
+  } else {
+    classname = 'closed'
+  }
+  console.log(classname)
+  return (
+    <button
+      className={`${styles.cell} ${styles[classname]}`}
+      alt={`cell-${classname}`}></button>
+  )
+}
